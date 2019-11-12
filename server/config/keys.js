6a85b96889f4fe3b70d9c5af0330ed697ebdf80f
Ryan Brutocao <ryanbrutocao@outlook.com>
2019-11-11 18:57:40 -0700

    updated files

---
+++ server/config/keys.js
@@ -0,0 +1,11 @@
+//export an object with keys of googleClientId, googleClientSecret,mongoURI, and cookieKey
+
+
+module.exports = {
+  googleClientID: "527119401095-flf80nen6cgthtcso628cp0tg59tmcij.apps.googleusercontent.com",
+  googleClientSecret: "sVerVPqfJK8kClGClpajTIMH",
+  mongoURI: "mongodb+srv://ryanb:colette12345@cluster0-aurkz.gcp.mongodb.net/test?retryWrites=true&w=majority",
+  cookieKey: "fnkds;lanksdf;aosdnf;asoddfin"
+};
+
+

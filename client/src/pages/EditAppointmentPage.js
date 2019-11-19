2afe717259aabf007a19c7dc528cc3b6db13fe78
brarver <brarver@yahoo.com>
2019-11-18 20:24:34 -0700

    appointment booking front end done

--- client/src/pages/EditAppointmentPage.js
+++ client/src/pages/EditAppointmentPage.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { editAppointment} from '../actions/appointments';
+import moment from 'moment'
 
 const EditAppointmentPage = (props) => {
+  let time = moment(props.appointment.startDate).format('dddd MMMM Do YYYY h:mm a');
   
   console.log('appointment: ' + JSON.stringify(props.appointment))
+
 
   return (
     <div>
       <p>{props.appointment.title}</p>
-      <p>{props.appointment.startDate} to {props.appointment.endDate}</p>
+      <p>{time}</p>
       <button onClick={() => {
-        props.dispatch(editAppointment({ id: props.appointment.id, }))
+        props.dispatch(editAppointment(props.appointment.id, {title: 'Booked'}))
+        props.history.push('/admin')
       }}>Book Appointment</button>
       
     </div>

bfc90c8d1a58327c9bddd347c6b62b10ec1bff5a
Colin D <colinv120@gmail.com>
2019-11-19 19:55:04 -0700

    can finally make an account in DB

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -30,7 +30,7 @@ module.exports = {
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  loginUser: function(req, res) {
+  logInUser: function(req, res) {
     db.Admin.findOne({ username: req.params.username })
       .then(data => res.json(data))
       .catch(err => res.status(422).json(err));

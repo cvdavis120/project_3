0a4902062356f8492df363503b7a9e4c56ee77e3
Colin D <colinv120@gmail.com>
2019-11-21 10:00:49 -0700

    fixed missing code

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -2,35 +2,37 @@ const db = require("../models");
 // Defining methods for the UsersController
 
 module.exports = {
-  findAll: function (req, res) {
+  findAll: function(req, res) {
     db.User.find(req.query)
       .sort({ date: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  findById: function (req, res) {
+  findById: function(req, res) {
     db.User.findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  update: function (req, res) {
+  update: function(req, res) {
     db.User.findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  remove: function (req, res) {
+  remove: function(req, res) {
     db.User.findById({ _id: req.params.id })
       .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  newAdmin: function (req, res) {
+  newAdmin: function(req, res) {
     console.log(req.body);
     db.Admin.create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.json(err));
   },
-  logInUser: function (req, res) {
+  logInUser: function(req, res) {
+    console.log("login user req data: ", req.query.email);
+    db.Admin.findOne({ email: req.query.email })
       .then(data => res.json(data))
       .catch(err => res.status(422).json(err));
   }

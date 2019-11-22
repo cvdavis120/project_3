99e94befea48a915c82d2a5a426c22e915e6c946
Colin D <colinv120@gmail.com>
2019-11-21 20:20:39 -0700

    backend of events works

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -3,8 +3,13 @@ const db = require("../models");
 
 module.exports = {
   addEvent: function(req, res) {
-    console.log(req.body)
+    console.log(req.body);
     db.Event.create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  getAllEvents: function(req, res) {
+    db.Event.find(req.query)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   }

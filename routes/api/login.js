c0d944511ccb8d3b68fa2e2e6ea443c8eeb0e73c
Colin D <colinv120@gmail.com>
2019-11-21 18:05:48 -0700

    working login

--- routes/api/login.js
+++ routes/api/login.js
@@ -6,12 +6,10 @@ router.route("/").get(loginController.logInUser);
 // router.route("/").get(loginController.findAll);
 
 // Matches with "/api/login/checkLoggedIn"
-router.route("/checkLoggedIn").get(loginController.logInUser);
+router.route("/checkLoggedIn").post(loginController.logInUser);
 
 // Matches /api/login/newadmin
 router.route("/newadmin").post(loginController.newAdmin);
-
-
 
 // Matches with "/api/admin/:id"
 // router.route("/admin").post(loginController.findById);
